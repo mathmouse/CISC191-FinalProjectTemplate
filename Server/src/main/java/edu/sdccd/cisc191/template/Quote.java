3b843384023b6589d179a6e555ac6aa00cb75f39
Sofia <sofia.demarco.en@gmail.com>
2023-11-13 00:03:18 -0800

    Architect 2

--- Server/src/main/java/edu/sdccd/cisc191/template/Quote.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/Quote.java
@@ -1,25 +1,20 @@
 package edu.sdccd.cisc191.template;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Quote {
-    private String[] quoteArray = {"Sofia says hi.", "DO YOUR HOMEWORK NOW NOW NOW", "There are snakes on the plane.", "Teddy says hi."};
+    private ArrayList<String> quoteList = new ArrayList<>();
     private Random randomGenerator = new Random();
 
-    /**
-     * Randomize a quote out of four options in an array
-     * @return a random String from an array
-     */
-    public String getQuote() {
-        int randomNum = randomGenerator.nextInt(4);
-        return quoteArray[randomNum];
+    public void setQuote(String s) {
+        quoteList.add(s);
     }
+    /**
 
-    /**
-     * For testing purposes: tests to see if array is recognized
-     * @return a String from index 1 of array above
-     */
-    public String getSecondQuote() {
-        return quoteArray[1];
-    }
+     Randomize a quote out of four options in an array
+     @return a random String from an array*/
+    public String getQuote() {
+        int randomNum = randomGenerator.nextInt(quoteList.size());
+        return quoteList.get(randomNum);}
 }
--- Server/src/test/java/edu/sdccd/cisc191/ServerTest.java
+++ Server/src/test/java/edu/sdccd/cisc191/ServerTest.java
@@ -3,46 +3,38 @@ package edu.sdccd.cisc191;
 
 import edu.sdccd.cisc191.template.*;
 import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ServerTest {
     @Test
-    void testQuoteArray() {
-        //test if quotes are recognized in array
+    void testQuoteArrayList() {
+        //test if quotes are properly added to and retrieved from arraylist
         Quote quote = new Quote();
-        String quoteLine = quote.getSecondQuote();
-        assertEquals(quoteLine, "DO YOUR HOMEWORK NOW NOW NOW");
+        quote.setQuote("Can't wait for vacation!");
+        String quoteLine = quote.getQuote();
+        assertEquals(quoteLine, "Can't wait for vacation!");
     }
+
     @Test
-    void test2DSnackArray() {
-        //test if dessert is able to maneuver between rows of 2d array - getFirst() method calls from row 0 and row 1
-        Dessert dessert = new Dessert();
-        String dessertLine = dessert.getFirst();
-        assertEquals(dessertLine, "Have some Nutella on White Bread");
-    }
-    @Test
-    void testGUI() {
-        //Can the app launch?
-    }
-    @Test
-    void testTimeConversion() {
-        //tests functionality of conversion methods in class Time
-        Time time = new Time();
-        time.setSecondsTotal(1);
-        assertEquals(time.getSecondsTotal(), 60);
-    }
-    @Test
-    void testAbstractClass() {
-        //test if sandwich recognizes method built off of abstract parent - getDrink() is built off abstract class Snack
-        Sandwich sandwich = new Sandwich();
-        String sandwichLine = sandwich.getFirst();
-        assertEquals(sandwichLine, "Make a sandwich with: White Bread, Salami\nand Cream Cheese");
-    }
-    @Test
-    void testInterfaceUrgent() {
-        //test if homework reacts as if it implements interface Urgent's property
-        Homework homework = new Homework("","", true, "");
-        assertEquals("*\n", homework.isUrgent());
+    void testNetwork() {
+        //setup for testNetwork in client
+        String response;
+        try {
+            ServerSocket serverSocket = new ServerSocket(8000);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+                response = "Sleep is for the weak";
+                out.writeObject(response);
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
